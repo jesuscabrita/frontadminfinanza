303663e5b87268a4052515d8d6e7f9679e96c48d
Jesus Cabrita <jesusarnaldo115@gmail.com>
2023-01-18 17:08:56 -0300

    front: api detalle

--- service/api.ts
+++ service/api.ts
@@ -2,5 +2,5 @@ import axios from "axios";
 
 export const api = axios.create({baseURL:'/'})
 export const api2 = axios.create({baseURL:'https://api.bluelytics.com.ar/'})
-export const api3 = axios.create({baseURL:"https://backadminfinanza.onrender.com/"})
+export const api3 = axios.create({baseURL:"http://localhost:5000"})
 export const api4 = axios.create({baseURL: '/apinoticias'})

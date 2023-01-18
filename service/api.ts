de896454b94d06aa3bd406c190495077a7334405
Jesus Cabrita <jesusarnaldo115@gmail.com>
2023-01-18 16:26:45 -0300

    front: api produccion

--- service/api.ts
+++ service/api.ts
@@ -4,6 +4,6 @@ export const api = axios.create({baseURL:'/'})
 
 export const api2 = axios.create({baseURL:'https://api.bluelytics.com.ar/'})
 
-export const api3 = axios.create({baseURL:"https://backadminfinanza.onrender.com"})
+export const api3 = axios.create({baseURL:"https://backadminfinanza.onrender.com/"})
 
 export const api4 = axios.create({baseURL: '/apinoticias'})
